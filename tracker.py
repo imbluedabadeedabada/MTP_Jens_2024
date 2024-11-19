26d5877e56d6f017794273244969380997c9ac29
imbluedabadeedabada <jensreijnen@gmail.com>
2024-11-19 10:47:47 +0100

    This seems to be working well

--- tracker.py
+++ tracker.py
@@ -49,7 +49,7 @@ def receive_rigid_body_frame(new_id, position, rotation):
         body = 'Glove'
     elif new_id == 5:
         body = 'Pepper'
-        # print("Orientation Pepper: ", current_orientation)
+        print("Orientation Pepper: ", current_orientation)
         # print("data: ", data)
     elif new_id == 1:
         body = 'Target_1'
@@ -62,102 +62,7 @@ def receive_rigid_body_frame(new_id, position, rotation):
             "position": current_position,
             "orientation": current_orientation
     }})
-    # time.sleep(0.1) ## Putting a sleep here seems to break the connection to the server
-
-def print_configuration(natnet_client):
-    #print("Connection Configuration:")
-    #print("  Client:          %s"% natnet_client.local_ip_address)
-    #print("  Server:          %s"% natnet_client.server_ip_address)
-    #print("  Command Port:    %d"% natnet_client.command_port)
-    #print("  Data Port:       %d"% natnet_client.data_port)
-
-    if natnet_client.use_multicast:
-        #print("  Using Multicast")
-        #print("  Multicast Group: %s"% natnet_client.multicast_address)
-        pass
-    else:
-        #print("  Using Unicast")
-        pass
-
-    #NatNet Server Info
-    application_name = natnet_client.get_application_name()
-    nat_net_requested_version = natnet_client.get_nat_net_requested_version()
-    nat_net_version_server = natnet_client.get_nat_net_version_server()
-    server_version = natnet_client.get_server_version()
-
-##    print("  NatNet Server Info")
-##    print("    Application Name %s" %(application_name))
-##    print("    NatNetVersion  %d %d %d %d"% (nat_net_version_server[0], nat_net_version_server[1], nat_net_version_server[2], nat_net_version_server[3]))
-##    print("    ServerVersion  %d %d %d %d"% (server_version[0], server_version[1], server_version[2], server_version[3]))
-##    print("  NatNet Bitstream Requested")
-##    print("    NatNetVersion  %d %d %d %d"% (nat_net_requested_version[0], nat_net_requested_version[1],\
-##       nat_net_requested_version[2], nat_net_requested_version[3]))
-    #print("command_socket = %s"%(str(natnet_client.command_socket)))
-    #print("data_socket    = %s"%(str(natnet_client.data_socket)))
-
-
-def print_commands(can_change_bitstream):
-    outstring = "Commands:\n"
-    outstring += "Return Data from Motive\n"
-    outstring += "  s  send data descriptions\n"
-    outstring += "  r  resume/start frame playback\n"
-    outstring += "  p  pause frame playback\n"
-    outstring += "     pause may require several seconds\n"
-    outstring += "     depending on the frame data size\n"
-    outstring += "Change Working Range\n"
-    outstring += "  o  reset Working Range to: start/current/end frame = 0/0/end of take\n"
-    outstring += "  w  set Working Range to: start/current/end frame = 1/100/1500\n"
-    outstring += "Return Data Display Modes\n"
-    outstring += "  j  print_level = 0 supress data description and mocap frame data\n"
-    outstring += "  k  print_level = 1 show data description and mocap frame data\n"
-    outstring += "  l  print_level = 20 show data description and every 20th mocap frame data\n"
-    outstring += "Change NatNet data stream version (Unicast only)\n"
-    outstring += "  3  Request 3.1 data stream (Unicast only)\n"
-    outstring += "  4  Request 4.0 data stream (Unicast only)\n"
-    outstring += "t  data structures self test (no motive/server interaction)\n"
-    outstring += "c  show configuration\n"
-    outstring += "h  print commands\n"
-    outstring += "q  quit\n"
-    outstring += "\n"
-    outstring += "NOTE: Motive frame playback will respond differently in\n"
-    outstring += "       Endpoint, Loop, and Bounce playback modes.\n"
-    outstring += "\n"
-    outstring += "EXAMPLE: PacketClient [serverIP [ clientIP [ Multicast/Unicast]]]\n"
-    outstring += "         PacketClient \"192.168.10.14\" \"192.168.10.14\" Multicast\n"
-    outstring += "         PacketClient \"127.0.0.1\" \"127.0.0.1\" u\n"
-    outstring += "\n"
-    #print(outstring)
-
-def add_lists(totals, totals_tmp):
-    totals[0]+=totals_tmp[0]
-    totals[1]+=totals_tmp[1]
-    totals[2]+=totals_tmp[2]
-    return totals
-
-def variables(voorwerp):
-
-    voorwerp = voorwerp
-    return voorwerp
-
-def request_data_descriptions(s_client):
-    # Request the model definitions
-    s_client.send_request(s_client.command_socket, s_client.NAT_REQUEST_MODELDEF,    "",  (s_client.server_ip_address, s_client.command_port) )
-
-def test_classes():
-    totals = [0,0,0]
-    print("Test Data Description Classes")
-    totals_tmp = DataDescriptions.test_all()
-    totals=add_lists(totals, totals_tmp)
-    print("")
-    print("Test MoCap Frame Classes")
-    totals_tmp = MoCapData.test_all()
-    totals=add_lists(totals, totals_tmp)
-    print("")
-    print("All Tests totals")
-    print("--------------------")
-    print("[PASS] Count = %3.1d"%totals[0])
-    print("[FAIL] Count = %3.1d"%totals[1])
-    print("[SKIP] Count = %3.1d"%totals[2])
+    # time.sleep(0.1) ## It seems that the data is not being updated in real-time, but in chronological order
 
 def my_parse_args(arg_list, args_dict):
     # set up base values
@@ -241,30 +146,7 @@ def do_command(sz_command):
     print("Command: %s - return_code: %d"% (sz_command, return_code) )
     return return_code
 
-def start_recording(name):
-    global file_name, is_recording, file
-    file_name = name
-    short_name = name.split('.')[0]
-    try:
-        #outfile=open(file_name,'w')
-        file = open(file_name,'a')
-        is_recording = True
-        do_command('StopRecording')
-        do_command("SetRecordTakeName," + short_name )
-        do_command("StartRecording")
-        return file    #was outfile
-    except:
-        return None
-        pass
 
-def stop_recording():
-    global file_name, is_recording, file
-    is_recording = False
-    do_command("StopRecording")
-    time.sleep(1)
-    file.close()
-    file_name = ""
-    
 
 if __name__=="__main__":
     s_client = init_tracker()
@@ -278,7 +160,7 @@ if __name__=="__main__":
         # count=10000
         while True:
             ws_server.send_message_to_all(json.dumps(data))
-            time.sleep(0.5)
+            time.sleep(0.2)
     except KeyboardInterrupt:
         pass
     except Exception as e:
